719d654d8f8f262cd5c39eab57ab25cce35358f1
Vitor Guidi <vitorguidi@gmail.com>
2024-06-16 17:01:30 -0300

    Adding impl for fill

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -9,3 +9,56 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+@KBD
+D=M
+@WRITE
+D;JGT
+@CLEAR
+D;JEQ
+
+(WRITE)
+    @R1
+    M=-1
+    @DRAW
+    0;JMP
+
+(CLEAR)
+    @R1
+    M=0
+    @DRAW
+    0;JMP
+
+(DRAW)
+    @SCREEN
+    D=A
+    @R2     //pixel to be painted
+    M=D
+
+    @8192   //number of iterations left
+    D=A  
+    @R3
+    M=D
+
+    @STEP
+    0;JMP
+
+(STEP)
+    @R1
+    D=M //pixel value
+    @R2
+    A=M
+    M=D
+
+    @R2
+    M=M+1
+    @R3
+    M=M-1
+
+    D=M
+    @STEP
+    D;JGT
+
+(LOOP)
+    @LOOP
+    0;JMP
